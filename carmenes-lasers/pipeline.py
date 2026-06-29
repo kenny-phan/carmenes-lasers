e6038acf398eca2cdf1af58bde781b35a84b9418
Kenny Phan <ktp@blpc0.bl.pvt>
2026-06-29 15:51:51 -0700

    scipy binning for polynomial fit

--- carmenes-lasers/pipeline.py
+++ carmenes-lasers/pipeline.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from astropy.modeling.models import Voigt1D
 from scipy.signal import find_peaks
+from scipy.stats import binned_statistic
 from specutils import Spectrum
 from specutils.manipulation import FluxConservingResampler
 from tqdm import tqdm
@@ -17,62 +18,21 @@ def check_spec_std(spec_arr, coeff=1):
     std_std = np.std(std_array)
     
     # Return boolean array where True indicates std > threshold
-    excess_std_mask = std_array > (std_mean + std_std)
-    return excess_std_mask
+    high_std_mask = std_array > (std_mean + std_std)
+    return high_std_mask
 
-def simple_threshold(flux, coeff):
-    return np.nanmedian(flux) + coeff * np.nanstd(flux)
+def replace_nan_with_median(new_spec_arr):
 
-def full_width_half_max(x, y, peakx, peaky, max_diff, verbose=False):
-    fwhm_arr = []
-    for i, peak in enumerate(peaky):
-        half_max = peak/2
-        center_freq = peakx[i]
-
-        debug_print(verbose, "center freq", center_freq)
-        x_args = np.where(np.abs(y - half_max) < max_diff)
-        x_vals = x[x_args]
-        # print("x vals", x_vals)
-        debug_print(verbose, f"There are {len(x_vals)} intersections between the spectrum and half max of this peak.")
-        lower_freq_arg = np.argmin(np.abs(center_freq - x_vals[x_vals < center_freq]))
-        upper_freq_arg = np.argmin(np.abs(center_freq - x_vals[x_vals > center_freq]))
-
-        lower_freq = x_vals[x_vals < center_freq][lower_freq_arg]
-        upper_freq = x_vals[x_vals > center_freq][upper_freq_arg]
-        debug_print(verbose, "lower, upper freqs", lower_freq, upper_freq)
-        fwhm = upper_freq - lower_freq
-        fwhm_arr.append(fwhm)
-        
-    return np.array(fwhm_arr)
-
-def spec_to_fwhms(spec, flux, sigma, max_diff=0.01, verbose=False):
-    threshold = simple_threshold(flux, sigma) # get the std threshold
-
-    peaks, _ = find_peaks(flux, threshold) # get peaks above threshold 
-    spec_pks, flx_pks = spec[peaks], flux[peaks]
+    no_nan_spec_arr = new_spec_arr.copy()  # Don't modify original
     
-    fwhms = full_width_half_max(spec, flux, spec_pks, flx_pks, max_diff, verbose=verbose) # fwhm of peaks
-
-    return fwhms
-
-def lsf_per_wav(spec, wl,
-                amplitude_L=1, 
-                w_lorentz=np.array([0.28, 0.21, 0.17]) * 1e-5, 
-                w_gauss=np.array([1.0, 1.01, 1.18]) * 1e-5):
+    # Calculate median per (order, obs) pair, shape (orders, 1, observations)
+    medians = np.nanmedian(new_spec_arr, axis=1, keepdims=True)
     
-    if wl <= 6000:
-        windex = 0
-    elif (wl > 6000) and (wl <= 9600):
-        windex = 1
-    elif wl > 9600:
-        windex = 2
+    # Replace non-finite values with median (broadcasts automatically)
+    no_nan_spec_arr = np.where(np.isfinite(new_spec_arr), new_spec_arr, medians)
+    
+    return no_nan_spec_arr
 
-    v1 = Voigt1D(x_0=wl, 
-                 amplitude_L=amplitude_L, 
-                 fwhm_L=w_lorentz[windex]*wl, 
-                 fwhm_G=w_gauss[windex]*wl)
-
-    return v1(spec)
 
 # ___DOPPLER CORRECT FOR EARTH ORBIT___
 def doppler_shift(wave_arr, velocity):
@@ -169,16 +129,28 @@ def resample_ords(shifted_wave_arr, spec_arr, resampler="fcr", save_dir=None):
     return new_wave_arr, new_spec_arr
 
 # ___REMOVE CONTINUUM__
-def polyfit(wave, spectra, degree=4):
-    med = np.nanmedian(spectra)
-    nonan_spectra = np.where(np.isfinite(spectra), spectra, med)  # replace NaN/Inf with median
 
-    z = np.polyfit(wave, nonan_spectra, degree)
+def midpoints(arr):
+    return (arr[:-1] + arr[1:]) / 2
+    
+def polyfit(wave, spectra, degree=4):
+
+    z = np.polyfit(wave, spectra, degree)
     p = np.poly1d(z)
 
     return p(wave)
 
-def remove_polyfit(new_wave, new_spec_arr, degree=4):
+def bin_order(new_wave, no_nan_spec, bins=50, degree=4):
+    bin_meds, bin_edges, binnumber = binned_statistic(new_wave, no_nan_spec, statistic='median', bins=bins)
+    bin_midpts = midpoints(bin_edges)
+    
+    bin_poly = polyfit(bin_midpts, bin_meds, degree=degree)
+    
+    poly = np.interp(new_wave, bin_midpts, bin_poly)
+
+    return poly
+
+def remove_polyfit(new_wave, new_spec_arr, bins=50, degree=4):
     """
     only for 1 order!
     Parameters
@@ -188,9 +160,70 @@ def remove_polyfit(new_wave, new_spec_arr, degree=4):
     poly_arr = np.empty_like(new_spec_arr)
     nopoly_arr = np.empty_like(new_spec_arr)
     for obs in range(new_spec_arr.shape[1]):
-        poly_arr[:, obs] = polyfit(new_wave, 
+        if bins: 
+            poly_arr[:, obs] = bin_order(new_wave, 
+                                       new_spec_arr[:, obs], 
+                                         bins=bins, 
+                                       degree=degree)
+        else: 
+            poly_arr[:, obs] = polyfit(new_wave, 
                                    new_spec_arr[:, obs], 
                                    degree=degree)
         nopoly_arr[:, obs] = new_spec_arr[:, obs]/poly_arr[:, obs]
 
     return poly_arr, nopoly_arr
+
+# ___LASER DETECTION___
+def simple_threshold(flux, coeff):
+    return np.nanmedian(flux) + coeff * np.nanstd(flux)
+
+def full_width_half_max(x, y, peakx, peaky, max_diff, verbose=False):
+    fwhm_arr = []
+    for i, peak in enumerate(peaky):
+        half_max = peak/2
+        center_freq = peakx[i]
+
+        debug_print(verbose, "center freq", center_freq)
+        x_args = np.where(np.abs(y - half_max) < max_diff)
+        x_vals = x[x_args]
+        # print("x vals", x_vals)
+        debug_print(verbose, f"There are {len(x_vals)} intersections between the spectrum and half max of this peak.")
+        lower_freq_arg = np.argmin(np.abs(center_freq - x_vals[x_vals < center_freq]))
+        upper_freq_arg = np.argmin(np.abs(center_freq - x_vals[x_vals > center_freq]))
+
+        lower_freq = x_vals[x_vals < center_freq][lower_freq_arg]
+        upper_freq = x_vals[x_vals > center_freq][upper_freq_arg]
+        debug_print(verbose, "lower, upper freqs", lower_freq, upper_freq)
+        fwhm = upper_freq - lower_freq
+        fwhm_arr.append(fwhm)
+        
+    return np.array(fwhm_arr)
+
+def spec_to_fwhms(spec, flux, sigma, max_diff=0.01, verbose=False):
+    threshold = simple_threshold(flux, sigma) # get the std threshold
+
+    peaks, _ = find_peaks(flux, threshold) # get peaks above threshold 
+    spec_pks, flx_pks = spec[peaks], flux[peaks]
+    
+    fwhms = full_width_half_max(spec, flux, spec_pks, flx_pks, max_diff, verbose=verbose) # fwhm of peaks
+
+    return fwhms
+
+def lsf_per_wav(spec, wl,
+                amplitude_L=1, 
+                w_lorentz=np.array([0.28, 0.21, 0.17]) * 1e-5, 
+                w_gauss=np.array([1.0, 1.01, 1.18]) * 1e-5):
+    
+    if wl <= 6000:
+        windex = 0
+    elif (wl > 6000) and (wl <= 9600):
+        windex = 1
+    elif wl > 9600:
+        windex = 2
+
+    v1 = Voigt1D(x_0=wl, 
+                 amplitude_L=amplitude_L, 
+                 fwhm_L=w_lorentz[windex]*wl, 
+                 fwhm_G=w_gauss[windex]*wl)
+
+    return v1(spec)
